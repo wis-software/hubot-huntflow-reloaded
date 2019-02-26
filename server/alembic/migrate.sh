1483886eeb63765aeb41d83fd7f5ac75162c23ac
BehindLoader <simonsuprun@gmail.com>
2019-02-26 17:24:44 +0300

    Add possibility to apply all migrations

--- server/alembic/migrate.sh
+++ server/alembic/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ALEMBIC_PATH="$(which alembic)"
-export POSTGRES_URL=${POSTGRES_URL="$2"}
-env PYTHONPATH=$(pwd) ${ALEMBIC_PATH} upgrade $1
+export POSTGRES_URL=${POSTGRES_URL="$1"}
+env PYTHONPATH=$(pwd) ${ALEMBIC_PATH} upgrade head
