d4f8a040fbe453055e92fca275a5df7bc2d67461
PopovaPolina <ppopova.wis@gmail.com>
2019-03-04 16:01:38 +0300

    Change way of searching for alimbic path

--- server/alembic/makemigrations.sh
+++ server/alembic/makemigrations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ALEMBIC_PATH="$(dirname "$(which python)" )/alembic"
+ALEMBIC_PATH="$(which alembic)"
 export POSTGRES_URL=${POSTGRES_URL="$2"}
 env PYTHONPATH=$(pwd) ${ALEMBIC_PATH} revision --autogenerate -m $1
